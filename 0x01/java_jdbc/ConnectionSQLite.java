3726a47da2fd630f59ffae20cea465e0267c80b6
Elton Henrique <eltonhst34@gmail.com>
2022-03-30 00:18:39 -0300

    teste

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -1,27 +1,27 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class ConnectionSQLite {
-    public static void createNewTable() {
-        String url = "jdbc:sqlite:database_test.db";
-        StringBuffer sql = new StringBuffer();
-        sql.append("CREATE TABLE IF NOT EXISTS cliente (");
-        sql.append("id integer PRIMARY KEY , ");
-        sql.append("nome text NOT NULL, ");
-        sql.append("idade integer, ");
-        sql.append("cpf text NOT NULL, ");
-        sql.append("rg text ");
-        sql.append(")");
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            stmt.execute(sql.toString());
+    public static void initConnection() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:database_test.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
     public static void main(String[] args) {
-        createNewTable();
+        initConnection();
     }
 }
